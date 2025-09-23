ebecec022b18d20788b7f452a703bdecd0642d89
Rahma1219 <rahmayoussef920@gmail.com>
2025-09-23 21:26:35 +0300

    freshcart

--- src/apis/allProducts.ts
+++ src/apis/allProducts.ts
@@ -2,9 +2,14 @@ import { Product } from "@/Types/Product.type";
 
 export default async function getAllProducts() {
   const response = await fetch(
-    "https://ecommerce.routemisr.com/api/v1/products",
+    `${process.env.NEXT_PUBLIC_API_URL}/products`,
     { cache: "no-store" }
   );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
   const { data }: { data: Product[] } = await response.json();
-return data;
+  return data;
 }
