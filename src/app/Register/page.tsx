636c818843f3200f90888327075aa8d95c7e9ec4
Rahma1219 <rahmayoussef920@gmail.com>
2025-09-23 18:40:01 +0300

    freshcart

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -37,10 +37,18 @@ const Register = () => {
 
   async function handleSubmit(values: RegisterSchemaType) {
     try {
-      const { data } = await axios.post(
+      const res = await fetch(
         "https://ecommerce.routemisr.com/api/v1/auth/signup",
-        values
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        }
       );
+
+      const data = await res.json();
 
       console.log(data);
       toast.success(data.message, {
@@ -66,99 +74,103 @@ const Register = () => {
   }
 
   return (
-   <>
-     <TitlePage title="Regiser" />
-    <div className="mx-auto px-10 md:px-0 w-full mt-12 md:w-1/2 mt-30">
-      <h1 className="text-3xl font-semibold">Register Now:</h1>
-      <div className="py-5 ">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleSubmit)}
-            className=" flex flex-col gap-3 "
-          >
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="md:text-[17px] font-normal">
-                    name:
-                  </FormLabel>
-                  <FormControl>
-                    <Input type="text" {...field} className="focus:" />
-                  </FormControl>
-                  <FormMessage />
-                  <FormDescription></FormDescription>
-                </FormItem>
-              )}
-            />
+    <>
+      <TitlePage title="Regiser" />
+      <div className="mx-auto px-10 md:px-0 w-full mt-12 md:w-1/2 mt-30">
+        <h1 className="text-3xl font-semibold">Register Now:</h1>
+        <div className="py-5 ">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className=" flex flex-col gap-3 "
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="md:text-[17px] font-normal">
+                      name:
+                    </FormLabel>
+                    <FormControl>
+                      <Input type="text" {...field} className="focus:" />
+                    </FormControl>
+                    <FormMessage />
+                    <FormDescription></FormDescription>
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="md:text-[17px] font-normal">
-                    email:
-                  </FormLabel>
-                  <FormControl>
-                    <Input type="email" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                  <FormDescription></FormDescription>
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="md:text-[17px] font-normal">
+                      email:
+                    </FormLabel>
+                    <FormControl>
+                      <Input type="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                    <FormDescription></FormDescription>
+                  </FormItem>
+                )}
+              />
 
-            <ShowPassword control={form.control} name="password" />
+              <ShowPassword control={form.control} name="password" />
 
-            <FormField
-              control={form.control}
-              name="rePassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="md:text-[17px] font-normal">
-                    rePassword:
-                  </FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      {...field}
-                      onPaste={(e) => e.preventDefault()}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                  <FormDescription></FormDescription>
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="rePassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="md:text-[17px] font-normal">
+                      rePassword:
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        {...field}
+                        onPaste={(e) => e.preventDefault()}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                    <FormDescription></FormDescription>
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="phone"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="md:text-[17px] font-normal">
-                    phone:
-                  </FormLabel>
-                  <FormControl>
-                    <Input type="tel" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                  <FormDescription></FormDescription>
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="md:text-[17px] font-normal">
+                      phone:
+                    </FormLabel>
+                    <FormControl>
+                      <Input type="tel" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                    <FormDescription></FormDescription>
+                  </FormItem>
+                )}
+              />
 
-            <div className="flex justify-end">
-              <Button type="submit" className="bg-green-600 hover:bg-green-600 cursor-pointer hover:scale-95 transition-all duration-200 ">
-                Register
-              </Button>
-            </div>
-          </form>
-        </Form>
+              <div className="flex justify-end">
+                <Button
+                  type="submit"
+                  className="bg-green-600 hover:bg-green-600 cursor-pointer hover:scale-95 transition-all duration-200 "
+                >
+                  Register
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
       </div>
-    </div></>
+    </>
   );
 };
 
