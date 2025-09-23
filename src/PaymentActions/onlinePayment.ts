7970c3af666405b2a6a12760676d244099073033
Rahma1219 <rahmayoussef920@gmail.com>
2025-09-23 21:02:31 +0300

    freshcart

--- src/PaymentActions/onlinePayment.ts
+++ src/PaymentActions/onlinePayment.ts
@@ -1,12 +1,13 @@
 import { getMyToken } from "@/utilities/token";
 
 export async  function onlinePaymentAction(id: string , values: object) {
+  const baseUrl = process.env.NEXT_URL!;
 
     const token = await getMyToken();
 
     if (!token) throw new Error("User not logged in");
 
-    const response = await fetch(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=http://localhost:3000`, {
+    const response = await fetch(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=${baseUrl}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
