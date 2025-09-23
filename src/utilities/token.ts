d69232bdc7647c310e395088d27e04037df80368
Rahma1219 <rahmayoussef920@gmail.com>
2025-09-23 22:19:27 +0300

    freshcart

--- src/utilities/token.ts
+++ src/utilities/token.ts
@@ -1,18 +1,31 @@
-"use server"
+// "use server"
 
-import { decode } from "next-auth/jwt";
-import { cookies } from "next/headers";
+// import { decode } from "next-auth/jwt";
+// import { cookies } from "next/headers";
 
-export async function getMyToken() {
-    const x = (await cookies()).get("next-auth.session-token")?.value;
+// export async function getMyToken() {
+//     const x = (await cookies()).get("next-auth.session-token")?.value;
 
 
-    const token = await decode({
-        token: x,
-        secret: process.env.NEXTAUTH_SECRET!
-    })
+//     const token = await decode({
+//         token: x,
+//         secret: process.env.NEXTAUTH_SECRET!
+//     })
 
-   return token?.token;
+//    return token?.token;
     
 
+// }
+
+"use server";
+
+import { authOption } from "@/auth";
+// import { authOption } from "@/app/api/auth/[...nextauth]/options"; 
+import { getServerSession } from "next-auth";
+
+export async function getMyToken() {
+  const session = await getServerSession(authOption);
+
+  // التوكن اللي رجعناه في jwt callback
+  return session?.token || null;
 }
